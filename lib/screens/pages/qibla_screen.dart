f01e41af0533d3686096165ef917bbda2bc0745a
fwdAndroid <fwdkaleem@gmail.com>
2025-11-02 23:15:53 +0500

    Removal

--- lib/screens/pages/qibla_screen.dart
+++ lib/screens/pages/qibla_screen.dart
@@ -1,9 +1,4 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/flutter_compass.dart';
-import 'package:flutter_qiblah/flutter_qiblah.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:quran_al_kareem/screens/widget/qibla_compass.dart';
 
 class QiblaScreen extends StatefulWidget {
   const QiblaScreen({super.key});
@@ -12,29 +7,9 @@ class QiblaScreen extends StatefulWidget {
   State<QiblaScreen> createState() => _QiblaScreenState();
 }
 
-class _QiblaScreenState extends State<QiblaScreen>
-    with SingleTickerProviderStateMixin {
+class _QiblaScreenState extends State<QiblaScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: FutureBuilder(
-          future: FlutterQiblah.androidDeviceSensorSupport(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(strokeWidth: 2),
-              );
-            }
-
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error.toString()}'));
-            }
-
-            return const QiblaCompass();
-          },
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
