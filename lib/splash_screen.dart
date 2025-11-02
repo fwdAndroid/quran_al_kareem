bcc6c30665fb1a08b67a57e5208bbcdfa4fc2a20
fwdAndroid <fwdkaleem@gmail.com>
2025-11-02 17:53:26 +0500

    first commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:azkar/screens/main/main_dashboard.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _checkAuth();
+  }
+
+  void _checkAuth() async {
+    await Future.delayed(const Duration(seconds: 3)); // Simulate splash time
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MainDashboard()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/bg.png"),
+            filterQuality: FilterQuality.high,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('assets/logo.png', height: 200),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
