fb8761a1b1baa4af9662672559dd00eaef784eaf
fwdAndroid <fwdkaleem@gmail.com>
2025-11-09 17:31:02 +0500

    Fawad

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 final mainColor = Color(0xffD4AF37);
+final buttonColor = Color(0xff3E2F0B);
+final primaryText = Colors.black;
+final secondaryTextColor = Colors.white;
+final alertColor = Colors.red;
+final iconColor = Colors.brown;
