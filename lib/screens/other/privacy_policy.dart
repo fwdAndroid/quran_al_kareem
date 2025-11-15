0996f9e8f7aa43aa17270acb92a8611f0442d1b1
fwdAndroid <fwdkaleem@gmail.com>
2025-11-15 14:30:43 +0500

    Fawad

--- lib/screens/other/privacy_policy.dart
+++ lib/screens/other/privacy_policy.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:quran_al_kareem/screens/widget/arabic_text_widget.dart';
 import 'package:quran_al_kareem/utils/colors.dart';
+import 'package:url_launcher/url_launcher.dart'; // 👈 Added for "Rate Us"
 
-class PrivacyPolicyScreen extends StatelessWidget {
+class PrivacyPolicyScreen extends StatefulWidget {
   const PrivacyPolicyScreen({super.key});
+
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  Future<void> _launchRateUs() async {
+    const url =
+        'https://ashaapp.online/privacy-policy-islampro/'; // change to your app ID
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Could not open Play Store')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +39,98 @@ class PrivacyPolicyScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            ArabicText(
-              "We respect your privacy and are committed to protecting your personal data. "
-              "This Privacy Policy explains how we handle your information when you use our app.\n\n"
-              "1. We do not collect any unnecessary personal data.\n"
-              "2. Any information such as login or subscription details are securely processed.\n"
-              "3. We do not share or sell your information to third parties.\n"
-              "4. Permissions (like storage or audio) are only used to enable app features.\n\n"
-              "By using this app, you consent to this Privacy Policy.",
-              style: TextStyle(fontSize: 16, height: 1.5, color: Colors.white),
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: primaryText, width: .5),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                children: [
+                  Icon(Icons.info_outline, size: 80.0, color: primaryText),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "Your Privacy Matters",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryText,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "We take your privacy seriously  and are commited to protected your personal information. We collect only the minimal data needed to provide and improve our services.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "For complete details about our data collection,usage and your rights. please read our full Privacy Policy.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: primaryText, width: .5),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                children: [
+                  Icon(Icons.language, size: 80.0, color: primaryText),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "View Full Privacy Policy",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryText,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Read our complete Privacy Policy\n on our website.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonColor,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      onPressed: _launchRateUs,
+                      child: const ArabicText(
+                        "View on Website",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/screens/other/terms_of_service.dart
+++ lib/screens/other/terms_of_service.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:quran_al_kareem/screens/widget/arabic_text_widget.dart';
 import 'package:quran_al_kareem/utils/colors.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class TermsAndServicesScreen extends StatelessWidget {
+class TermsAndServicesScreen extends StatefulWidget {
   const TermsAndServicesScreen({super.key});
+
+  @override
+  State<TermsAndServicesScreen> createState() => _TermsAndServicesScreenState();
+}
+
+class _TermsAndServicesScreenState extends State<TermsAndServicesScreen> {
+  Future<void> _launchRateUs() async {
+    const url =
+        'https://ashaapp.online/terms-of-service-tos-islampro/'; // change to your app ID
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Could not open Play Store')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: mainColor,
-
       appBar: AppBar(
         title: ArabicText(
-          "Terms & Services",
+          "Terms of Service",
           style: TextStyle(color: primaryText, fontWeight: FontWeight.bold),
         ),
         backgroundColor: mainColor,
@@ -22,15 +39,98 @@ class TermsAndServicesScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            ArabicText(
-              "By using this application, you agree to the following terms and conditions:\n\n"
-              "1. The app is intended for personal, non-commercial Islamic education and guidance.\n"
-              "2. All Quran, Hadith, and Dua content is sourced from authentic references.\n"
-              "3. You may not copy, modify, or redistribute any part of the app without permission.\n"
-              "4. The app developers are not responsible for any misuse or interpretation of the content.\n\n"
-              "Continued use of the app means you agree to these terms.",
-              style: TextStyle(fontSize: 16, height: 1.5, color: Colors.white),
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: primaryText, width: .5),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                children: [
+                  Icon(Icons.info_outline, size: 80.0, color: primaryText),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "Terms of Service Overview",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryText,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "We take your terms of service seriously  and are commited to protected your personal information. We collect only the minimal data needed to provide and improve our services.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "For complete details about our data collection,usage and your rights. please read our full Terms of Service.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: primaryText, width: .5),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: Column(
+                children: [
+                  Icon(Icons.language, size: 80.0, color: primaryText),
+                  SizedBox(height: 16.0),
+                  Text(
+                    "View Full Terms of Service",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryText,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Read our complete Terms of Service\n on our website.",
+                      style: TextStyle(fontSize: 16.0, color: primaryText),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonColor,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      onPressed: _launchRateUs,
+                      child: const ArabicText(
+                        "View on Website",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
